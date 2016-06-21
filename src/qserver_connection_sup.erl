871ef22dc5e2668026f5616fc16da033232b20a5
Ino Murko <ino.murko@outlook.com>
2016-06-21 19:38:59 +0100

    docs

--- src/qserver_connection_sup.erl
+++ src/qserver_connection_sup.erl
@@ -15,10 +15,15 @@ init([]) ->
     MaxSecondsBetweenRestarts = 3600,
     SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
     {ok, {SupFlags,
-          [{connection_id, {ProtocolProcessor, start_link, []}, temporary, 1000, worker, [ProtocolProcessor]} 
-          ]}
+          [{connection_id, {ProtocolProcessor, start_link, []}, temporary, 1000, worker, [ProtocolProcessor]}]
+          }
     }.
 
+%% @doc In the case of a simple_one_for_one supervisor, the child specification defined in Module:init/1 will be used, 
+%% and ChildSpec shall instead be an arbitrary list of terms List. The child process will then be started by appending List
+%% to the existing start function arguments, i.e. by calling apply(M, F, A++List) where {M,F,A} is the start function defined in 
+%% the child specification.
+%% @end
 start_child(Socket) ->
-    supervisor:start_child(?MODULE, [Socket]).
+    supervisor:start_child(?MODULE, [Socket]),
     
